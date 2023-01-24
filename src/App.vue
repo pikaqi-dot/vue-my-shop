5a7d98d531ca8d3a1fce298db52b858bb30ecc72
surface pro 3 <hual83129@gmail.com>
2023-01-24 14:26:32 +0800

    1-24/14:26

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <div id="app">
+    <v-header :seller="seller"></v-header>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+import header from "./components/header";
+import axios from "axios";
+export default {
+  name: "App",
+  components: {
+    "v-header": header,
+  },
+  data() {
+    return {
+      seller: {},
+    };
+  },
+  created() {
+    axios.get("./static/data.json").then((res) => {
+      this.seller = res.data.seller;
+      console.log(this.seller);
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
